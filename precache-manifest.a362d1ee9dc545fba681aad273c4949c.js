3491507a2673ac3c6ac7f838744cd38e656ae94a
atorenherrinton <atorenherrinton@gmail.com>
2020-03-23 10:18:10 -0700

    Updates

---
+++ precache-manifest.a362d1ee9dc545fba681aad273c4949c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0dbe98519d957bbabe64d9f446e0a822",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "8ac4b1f19b9181bbb647",
+    "url": "/monsters-rolodex/static/css/main.5772374e.chunk.css"
+  },
+  {
+    "revision": "30436d15d072549122d4",
+    "url": "/monsters-rolodex/static/js/2.0d232ef2.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.0d232ef2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ac4b1f19b9181bbb647",
+    "url": "/monsters-rolodex/static/js/main.71c5aeaa.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
